a3cb33fd964a58b81bff398475ae60eec644b54a
MarcoGr11 <babyn.bohdan.o@chnu.edu.ua>
2024-05-02 16:51:16 +0300

    add a little change in a code

--- lab_7dvoz.h
+++ lab_7dvoz.h
@@ -1,157 +1,172 @@
 #include <iostream>
 using namespace std;
 #include "memory"
-
 using namespace std;
 
+
 template<typename T>
-struct Node2 {
+struct Node{
     T data;
-    shared_ptr<Node2<T>> next;
-    shared_ptr<Node2<T>> prev;
+    shared_ptr<Node<T>> next;
+
+    Node(const T& value) : data(value), next(nullptr) {}
+
 };
 
 template<typename T>
-class DoubleList {
+class SingleList{
 private:
-    shared_ptr<Node2<T>> head;
-    shared_ptr<Node2<T>> tail;
+    shared_ptr<Node<T>> head;
     size_t size;
-
-    shared_ptr<Node2<T>> getNode(size_t index) const; //допомагає нам отримувати доступ до вузлів у списку за їхнім порядковим номером.
-
 public:
-    DoubleList() : head(nullptr), tail(nullptr), size(0) {}
 
-    //---Метод вставки---
-    void pushBack(const T& value);
+    SingleList():head(nullptr),size(0){}
+
+    //--- Методи вставки---
     void pushFront(const T& value);
+    void pushBack(const T& value);
 
-    //---Метод видалення--
-    void popBack();
+    //--- Методи видалення---
     void popFront();
+    void popBack();
 
-    //---Додавання та віднімання за індексом--
-    void insertAt(size_t index, const T& value);
+    //---Доступ до елементів---
+    T& operator[](size_t index)const;
+
+    //---Додавання та віднімання за індексом---
+    void insertAt(size_t index,const T& value);
     void removeAt(size_t index);
 
-    //---Методи для роботи з розміром---
-    size_t Size() const { return size; }
-    bool isEmpty() const { return size == 0; }
+    //--Методи для роботи з розміром---
+    bool isEmpty() const;
+    size_t Size() const;
 
     //---Метод пошуку---
-    bool Find(const T& value) const;
+    bool Find(const T& value)const;
 
-    //--Метод доступу за індексом---
-    T& operator[](size_t index) const;
-
-    //---Stream insertion---
-    friend ostream& operator<<(ostream& os, const DoubleList<T>& list) {
-        shared_ptr<Node2<T>> current = list.head;
-        while (current != nullptr) {
-            os << current->data << " ";
+    //--- friend operators---
+    friend ostream& operator<<(ostream& os, const SingleList<T> list){
+        shared_ptr<Node<T>> current = list.head;
+        while (current != nullptr){
+            os << current->data<< " ";
             current = current->next;
         }
         return os;
     }
 
+    //--- iterators methods---
+//    Iterator Begin();
+//
+//    Iterator End();
+
+
 };
 
+
 template<typename T>
-void DoubleList<T>::pushBack(const T& value) {
-    unique_ptr<Node2<T>> newNode = make_unique<Node2<T>>();
-    newNode->data = value;
-    newNode->next = nullptr;
-    newNode->prev = tail;
-    if (tail != nullptr) {
-        tail->next = move(newNode);
-        tail = tail->next;
-    } else {
+void SingleList<T>::pushBack(const T& value) {
+    unique_ptr<Node<T>> newNode = make_unique<Node<T>>(value);
+    if (head == nullptr) {
         head = move(newNode);
-        tail = head;
+    } else {
+        shared_ptr<Node<T>> current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = move(newNode);
     }
     size++;
 }
 
 template<typename T>
-void DoubleList<T>::pushFront(const T& value) {
-    unique_ptr<Node2<T>> newNode = make_unique<Node2<T>>();
-    newNode->data = value;
-    newNode->next = head;
-    newNode->prev = nullptr;
-    if (head != nullptr) {
-        head->prev = move(newNode);
-    } else {
-        tail = move(newNode);
-    }
-    head = newNode->prev;
+void SingleList<T>::pushFront(const T& value) {
+    unique_ptr<Node<T>> newNode = make_unique<Node<T>>(value);
+    newNode->next = move(head);
+    head = move(newNode);
     size++;
 }
 
 template<typename T>
-void DoubleList<T>::popBack() {
-    if (tail == nullptr)
-        throw out_of_range("List is empty");
-    if (head == tail) {
-        head.reset();
-    } else {
-        tail->prev->next.reset();
-    }
-    tail = tail->prev;
-    size--;
-}
-
-template<typename T>
-void DoubleList<T>::popFront() {
+void SingleList<T>::popBack() {
     if (head == nullptr)
         throw out_of_range("List is empty");
-    if (head == tail) {
-        tail.reset();
+    if (head->next == nullptr) {
+        head.reset();
     } else {
-        head->next->prev.reset();
+        shared_ptr<Node<T>> current = head;
+        while (current->next->next != nullptr) {
+            current = current->next;
+        }
+        current->next.reset();
     }
-    head = head->next;
     size--;
 }
 
 template<typename T>
-void DoubleList<T>::insertAt(size_t index, const T& value) {
+void SingleList<T>::popFront() {
+    if (head == nullptr)
+        throw out_of_range("List is empty");
+    head = move(head->next);
+    size--;
+}
+
+template<typename T>
+T& SingleList<T>::operator[](size_t index) const {
+    if (index >= size)
+        throw out_of_range("Index out of range");
+    shared_ptr<Node<T>> current = head;
+    for (size_t i = 0; i < index; ++i) {
+        current = current->next;
+    }
+    return current->data;
+}
+
+template<typename T>
+void SingleList<T>::insertAt(size_t index, const T& value) {
     if (index > size)
         throw out_of_range("Index out of range");
     if (index == 0) {
         pushFront(value);
-    } else if (index == size) {
-        pushBack(value);
     } else {
-        shared_ptr<Node2<T>> current = getNode(index);
-        unique_ptr<Node2<T>> newNode = make_unique<Node2<T>>();
-        newNode->data = value;
-        newNode->next = current;
-        newNode->prev = current->prev;
-        current->prev->next.reset(newNode.release());   // повертає вказівник який зберігається в unique_ptr і забирає його
-        current->prev = current->prev->next;
+        shared_ptr<Node<T>> newNode = make_shared<Node<T>>(value);
+        shared_ptr<Node<T>> current = head;
+        for (size_t i = 0; i < index - 1; ++i) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
         size++;
     }
 }
 
 template<typename T>
-void DoubleList<T>::removeAt(size_t index) {
+void SingleList<T>::removeAt(size_t index) {
     if (index >= size)
         throw out_of_range("Index out of range");
     if (index == 0) {
         popFront();
-    } else if (index == size - 1) {
-        popBack();
     } else {
-        shared_ptr<Node2<T>> current = getNode(index);
-        current->prev->next = current->next;
-        current->next->prev = current->prev;
+        shared_ptr<Node<T>> current = head;
+        for (size_t i = 0; i < index - 1; ++i) {
+            current = current->next;
+        }
+        current->next = current->next->next;
         size--;
     }
 }
+
 template<typename T>
-bool DoubleList<T>::Find(const T& value) const {
-    shared_ptr<Node2<T>> current = head;
+size_t SingleList<T>::Size() const {
+    return size;
+}
+
+template<typename T>
+bool SingleList<T>::isEmpty() const {
+    return size == 0;
+}
+template<typename T>
+bool SingleList<T>::Find(const T& value) const {
+    shared_ptr<Node<T>> current = head;
     while (current != nullptr) {
         if (current->data == value) {
             return true;
@@ -159,22 +174,4 @@ bool DoubleList<T>::Find(const T& value) const {
         current = current->next;
     }
     return false;
-}
-template<typename T>
-T& DoubleList<T>::operator[](size_t index) const {
-    if (index >= size)
-        throw out_of_range("Index out of range");
-    shared_ptr<Node2<T>> current = getNode(index);
-    return current->data;
-}
-
-template<typename T>
-shared_ptr<Node2<T>> DoubleList<T>::getNode(size_t index) const {
-    if (index >= size)
-        throw out_of_range("Index out of range");
-    shared_ptr<Node2<T>> current = head;
-    for (size_t i = 0; i < index; ++i) {
-        current = current->next;
-    }
-    return current;
 }
